3c105e57d55fb824bbc286d21c6ab40ba2a5aaad
gx2151 <77954776+gx2151@users.noreply.github.com>
2021-04-13 14:22:12 +0800

    Update forms.py

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,26 +1,5 @@
 from django.forms import ModelForm
 from .models import Squirrel
 
-class AddSightingsForm(ModelForm):
-    class Meta:
-        model = Squirrel
-        fields = [
-                'X',
-                'Y',
-                'Unique_Squirrel_ID',
-                'Shift',
-                'Date'
-        ]
 
-class UpdateForm(ModelForm):
-    class Meta:
-        model = Squirrel
-        fields = [
-                'X',
-                'Y',
-                'Unique_Squirrel_ID',
-                'Shift',
-                'Date',
-                'Age'
-                ]
 
